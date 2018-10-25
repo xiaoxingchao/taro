d455e269664ce04974dd8dcc40b0b28ca9093589
xxc <549021324@qq.com>
2018-10-25 21:56:43 +0800

    规则

--- src/pages/answerrule/answerrule.js
+++ src/pages/answerrule/answerrule.js
@@ -1,22 +1,13 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View,Text } from '@tarojs/components'
-// import { AtForm,AtInput,AtButton  } from 'taro-ui'
-import Bottom from '../component/bottom/index'
+import Bottom from '../component/bottom/bottom'
 import './answerrule.less'
 
 export default class Index extends Component {
-
-  config = {
-    navigationBarTitleText: '答题规则',
-    // 定义需要引入的第三方组件
-    // usingComponents: {
-    //   "van-button": "../../components/vant-weapp/dist/button/index" // 书写第三方组件的相对路径
-    // }
-  }
   constructor(props){
     super(props);
     this.state={
-      
+
     }
   }
 
@@ -30,8 +21,6 @@ export default class Index extends Component {
   componentDidShow () { }
 
   componentDidHide () { }
-  getinfo=()=>{
-  }
   render () {
     return (
       <View className='con'>
