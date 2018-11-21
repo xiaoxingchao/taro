498e464f2a278df59eb8aa6b0cd2907aa308543f
xiaoxingchao <549021324@qq.com>
2018-11-21 18:10:33 +0800

    规则

--- src/pages/answerrule/answerrule.js
+++ src/pages/answerrule/answerrule.js
@@ -1,32 +1,68 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View,Text } from '@tarojs/components'
+import { View  } from '@tarojs/components'
 import Bottom from '../component/bottom/bottom'
+import api from '../../service/api'
+import {showModel} from '../../utils/tools'
 import './answerrule.less'
+// import { userlist } from '../../actions/counter'
+import Loading from '../component/loading/loading'
+import Login from '../component/login/login'
+import WxParse from '../../wxParse/wxParse'
+
 
 export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '积分详情',
+  }
   constructor(props){
     super(props);
     this.state={
-
+      isload:true,
+      data:{}
     }
   }
 
   componentWillMount () { }
 
   componentDidMount () {
-
+    this.setState({
+      isload:false,
+    })
   }
   componentWillUnmount () { }
 
-  componentDidShow () { }
+  componentDidShow () { 
+    api.post('jsonapi/wx_app/getRule.json', {}).then((res) => {
+      if (res.data.code == 0) {
+        var data = res.data.result;
+        WxParse.wxParse('jd_rule', 'html', data.jd_rule, this.$scope, 5);
+        WxParse.wxParse('jj_rule', 'html', data.jj_rule, this.$scope, 5);
+        WxParse.wxParse('db_rule', 'html', data.db_rule, this.$scope, 5);
+        this.setState({
+          data:res.data.result?res.data.result:{},
+        })
+      } else {
+        showModel(JSON.stringify(res.errMsg))
+      }
+    }).catch((errMsg) => {
+      showModel('网络连接失败' + JSON.stringify(errMsg))
+    })
+  }
 
   componentDidHide () { }
+
   render () {
+    let {data} = this.state;
     return (
       <View className='con'>
-        <Text>规则;.......</Text>
-
+        <View className='address'>
+          
+          
+        </View>
         <Bottom></Bottom>
+        <Login />
+        <Loading load={this.state.isload} />
       </View>
     )
   }
--- src/pages/scoredetails/scoredetails.js
+++ src/pages/scoredetails/scoredetails.js
@@ -32,12 +32,10 @@ export default class Index extends Component {
   componentWillUnmount () { }
 
   componentDidShow () { 
-    let userId = Taro.getStorageSync('userId');
-    if(!userId) return;
-    api.post('jsonapi/iwebshop_member/get.json', {user_id:userId}).then((res) => {
+    api.post('jsonapi/wx_app/getRule.json', {}).then((res) => {
       if (res.data.code == 0) {
         this.setState({
-          data:res.data.data?res.data.data[0]:{},
+          data:res.data.result?res.data.result:{},
         })
       } else {
         showModel(JSON.stringify(res.errMsg))
@@ -48,38 +46,7 @@ export default class Index extends Component {
   }
 
   componentDidHide () { }
-  formSubmit=(e)=>{
-    let userId = Taro.getStorageSync('userId');
-    let {data} = this.state;
-    var value = e.detail.value;
-    for(var i in value){
-      if(value[i]==''){
-        showModel("请填写完整");
-        return;
-      }
-    }
-    if(JSON.stringify(data)==='{}'){
-      api.post('jsonapi/iwebshop_member/add.json', {...{user_id:userId},...value}).then((res) => {
-        if (res.data.code == 0) {
-          Taro.navigateBack();
-        } else {
-          showModel(JSON.stringify(res.errMsg))
-        }
-      }).catch((errMsg) => {
-        showModel('网络连接失败' + JSON.stringify(errMsg))
-      })
-    }else{
-      api.post('jsonapi/iwebshop_member/update.json', {...{ID:data.ID},...value}).then((res) => {
-        if (res.data.code == 0) {
-          Taro.navigateBack();
-        } else {
-          showModel(JSON.stringify(res.errMsg))
-        }
-      }).catch((errMsg) => {
-        showModel('网络连接失败' + JSON.stringify(errMsg))
-      })
-    }
-  }
+
   render () {
     let {data} = this.state;
     return (
