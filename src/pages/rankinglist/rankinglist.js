e4d3b548a33f26a0f15adff476274a1a348793eb
xiaoxingchao <549021324@qq.com>
2018-11-28 17:15:43 +0800

    排行榜完成

--- src/pages/rankinglist/rankinglist.js
+++ src/pages/rankinglist/rankinglist.js
@@ -6,7 +6,7 @@ import api from '../../service/api'
 import './rankinglist.less'
 import Login from '../component/login/login'
 import Loading from '../component/loading/loading'
-import {showModel,rootUrl} from '../../utils/tools'
+import {showModel} from '../../utils/tools'
 import pmtwo from '../image/pmtwo.png'
 import pmone from '../image/pmone.png'
 import pmthree from '../image/pmthree.png'
@@ -26,11 +26,12 @@ export default class Index extends Component {
     super(props);
     this.state={
       isload:true,
-      type:'1',
+      type:'W',
       data:[],
+      page:1,
+      count:0
     }
   }
-
   componentWillMount () { }
 
   componentDidMount () {
@@ -41,18 +42,39 @@ export default class Index extends Component {
   componentWillUnmount () {
 
   }
-
-  componentDidShow () { 
-    api.post('jsonapi/iwebshop_question_zt/getA.json', {}).then((res) => {
+  getResult=(parame,more)=>{
+    api.post('jsonapi/iwebshop_score/order.json', parame).then((res) => {
       if (res.data.code == 0) {
+        let data = [];
+        if(more===1){
+          data=[...this.state.data,...res.data.data]
+        }else{
+          data=res.data.data;
+        }
         this.setState({
-          data:res.data.data?res.data.data:[],
+          data:data,
+          count:res.data.count
         })
       } else {
         showModel(JSON.stringify(res.errMsg))
       }
     }).catch((errMsg) => {
       showModel('网络连接失败' + JSON.stringify(errMsg))
+    })
+  }
+  handleResult=(more)=>{
+    let {page} = this.state;
+    let p = {};
+    p.__start = (page-1)*6;
+    p.__size = 6;
+    p.__count = true;
+    this.getResult({...p,...{orderType:this.state.type}},more);
+  }
+  componentDidShow () { 
+    this.setState({
+      page:1
+    },()=>{
+      this.handleResult(0);
     })
   }
 
@@ -64,21 +86,31 @@ export default class Index extends Component {
       scrollTop: 0
     })
   }
+  more=()=>{
+    this.setState({
+      page:this.state.page+1
+    },()=>{
+      this.handleResult(1);
+    })
+  }
   rank=(type)=>{
     this.setState({
+      page:1,
       type:type
+    },()=>{
+      this.handleResult(0);
     })
   }
   render () {
-    let {data,type} = this.state;
+    let {data,type,count} = this.state;
     return (
       <View className='con'>
         <View className='headbox'>
           <View>
             <View className='control'>
-              <View className={`week-rank control-rank+${type==='1'?' rank-active':''}`} onClick={this.rank.bind(this,'1')}>周排行</View>
-              <View className={`month-rank control-rank+${type==='2'?' rank-active':''}`} onClick={this.rank.bind(this,'2')}>月排行</View>
-              <View className={`score-rank control-rank+${type==='3'?' rank-active':''}`} onClick={this.rank.bind(this,'3')}>积分排名</View>
+              <View className={`week-rank control-rank+${type==='W'?' rank-active':''}`} onClick={this.rank.bind(this,'W')}>周排行</View>
+              <View className={`month-rank control-rank+${type==='M'?' rank-active':''}`} onClick={this.rank.bind(this,'M')}>月排行</View>
+              <View className={`score-rank control-rank+${type==='A'?' rank-active':''}`} onClick={this.rank.bind(this,'A')}>积分排名</View>
             </View>
           </View>
         </View>
@@ -92,79 +124,84 @@ export default class Index extends Component {
             <View className='content-ul1'>
               <View className='content-li1 two'>
                 <View className='li1-avatar'>
-                  <Image src='https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoyU4qpD8MmuIGUpaHVdBXPpCVuiayToyzicUTYlSVVIkiaTBa5lzsCgpMXIKy2ArQB2sWan2rKX43Tg/132' className='avatar'></Image>
+                  <Image src={data[1].avatarUrl} className='avatar'></Image>
                   <Image src={pmtwo} className='avatar-two'></Image>
                 </View>
                 <View className='li1-name'>
-                  live
+                  {data[1].nickName}
                 </View>
                 <View className='li1-score'>
-                  最高积分: 100
+                  最高积分: {data[1].allScore}
                 </View>
                 <View className='li1-ques'>
-                  最高答题数: 100
+                  最高答题数: {data[1].allScore}
                 </View>
               </View>
               <View className='content-li1 one'>
                 <View className='li1-avatar'>
-                  <Image src='https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoyU4qpD8MmuIGUpaHVdBXPpCVuiayToyzicUTYlSVVIkiaTBa5lzsCgpMXIKy2ArQB2sWan2rKX43Tg/132' className='avatar'></Image>
+                  <Image src={data[0].avatarUrl} className='avatar'></Image>
                   <Image src={pmone} className='avatar-two'></Image>
                 </View>
                 <View className='li1-name'>
-                  live
+                  {data[0].nickName}
                 </View>
                 <View className='li1-score'>
-                  最高积分: 100
+                  最高积分: {data[0].allScore}
                 </View>
                 <View className='li1-ques'>
-                  最高答题数: 100
+                  最高答题数: {data[0].allScore}
                 </View>
               </View>
               <View className='content-li1 three'>
                 <View className='li1-avatar'>
-                  <Image src='https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoyU4qpD8MmuIGUpaHVdBXPpCVuiayToyzicUTYlSVVIkiaTBa5lzsCgpMXIKy2ArQB2sWan2rKX43Tg/132' className='avatar'></Image>
+                  <Image src={data[2].avatarUrl} className='avatar'></Image>
                   <Image src={pmthree} className='avatar-two'></Image>
                 </View>
                 <View className='li1-name'>
-                  live
+                  {data[2].nickName}
                 </View>
                 <View className='li1-score'>
-                  最高积分: 100
+                  最高积分: {data[2].allScore}
                 </View>
                 <View className='li1-ques'>
-                  最高答题数: 100
+                  最高答题数: {data[2].allScore}
                 </View>
               </View>
             </View>
             <View className='content-ul2'>
-              <View className='content-li2'>
-                <View className='phbimg'>
-                  <Image src={phbbgimg} className='phbimg-img' />
-                  NO100
-                </View>
-                <View className='li2_divL'>
-                  <Image src='https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoyU4qpD8MmuIGUpaHVdBXPpCVuiayToyzicUTYlSVVIkiaTBa5lzsCgpMXIKy2ArQB2sWan2rKX43Tg/132' />
-                </View>
-                <View className='li2_divM'>
-                  <View className='li2_pT'>
-                    live
-                  </View>
-                  <View className='li2_pB'>
-                    最高答题数: 4
-                  </View>
-                </View>
-                <View className='li2_divR'>
-                  最高积分: 8
-                </View>
-              </View>
+              {
+                data.map((item,index)=>{
+                  return <View key={index}>
+                    {index>=3?<View className='content-li2' >
+                      <View className='phbimg'>
+                        <Image src={phbbgimg} className='phbimg-img' />
+                        NO{index+1}
+                      </View>
+                      <View className='li2_divL'>
+                        <Image src={item.avatarUrl} />
+                      </View>
+                      <View className='li2_divM'>
+                        <View className='li2_pT'>
+                          {item.nickName}
+                        </View>
+                        <View className='li2_pB'>
+                          最高答题数: {item.allScore}
+                        </View>
+                      </View>
+                      <View className='li2_divR'>
+                        最高积分: {item.allScore}
+                      </View>
+                    </View>:''}
+                  </View> 
+                })
+              }
             </View>
           </View>
           <View>
             {
-              1?<View className='more'>点击加载更多</View>:<View className='more'>到底了</View>
+              data.length<count?<View className='more' onClick={this.more.bind(this)}>点击加载更多</View>:<View className='more'>到底了</View>
             }
           </View>
-          
           <Bottom></Bottom>
         </ScrollView>
         <Login />
